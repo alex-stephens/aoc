c341604481e5b84fcbf7a8186c20408924abb0e0
Alex <alexstephens9@gmail.com>
2020-12-12 10:00:58 +0000

    Day 12 - code almost unchanged for part 2

---
+++ 12/b.py
@@ -0,0 +1,29 @@
+actions = []
+with open('input.txt') as f:
+    for line in f.readlines():
+        actions.append(line.strip())
+
+dirs = {'N':(0,1), 'E':(1,0), 'S':(0,-1), 'W':(-1,0)}
+p = (0,0) # current position
+h = (10,1) # current heading (now represents the waypoint)
+
+for a in actions:
+    c, val = a[0], int(a[1:])
+
+    # Direction update
+    if c in ('L,R'):
+        if a in ('L90', 'R270'):
+            h = (-h[1], h[0])
+        elif a in ('R90', 'L270'):
+            h = (h[1], -h[0])
+        elif a in ('L180', 'R180'):
+            h = (-h[0], -h[1])
+        continue
+
+    if c == 'F':
+        p = (p[0] + h[0] * val, p[1] + h[1] * val)
+    else: # NESW - move the waypoint
+        b = dirs[c] # bearing
+        h = (h[0] + b[0] * val, h[1] + b[1] * val)
+    
+print(sum([abs(x) for x in p]))
