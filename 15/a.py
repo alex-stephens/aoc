42a1cea4696823ed5d13481fc75c2d715f1d35d8
Alex <alexstephens9@gmail.com>
2020-12-15 09:04:18 +0000

    Day 15

---
+++ 15/a.py
@@ -0,0 +1,24 @@
+num = []
+with open('input.txt') as f:
+    num = list(map(int, f.readline().split(',')))
+memory = {}
+
+for i, n in enumerate(num):
+    if n not in memory:
+        memory[n] = [i]
+    else:
+        memory[n].append(i)
+    last = n
+
+for i in range(len(num), 2020):
+    if len(memory[last]) == 1:
+        nxt = 0
+        memory[nxt].append(i)
+    else:
+        nxt = memory[last][-1] - memory[last][-2]
+        if nxt not in memory:
+            memory[nxt] = []
+        memory[nxt].append(i)
+    last = nxt
+
+print(last)
